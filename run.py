b10dae02d02a2a68e84a3df0dd7044f46b0f21cd
Danvers Trenhaile <45981790+djtrenhaile5757@users.noreply.github.com>
2018-12-25 21:25:04 -0600

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import subprocess as s
 import shlex as sh
 
 
-path = "/Users/dtrenhaile/Desktop/"
-top_dir = path + "test"# "1995_white_Chevy_Sonic_copy"
+path = #path_to_directories
+top_dir = path + "test" 
 save_dir = path + "save"
 
 s.call(sh.split("python main.py --directory " + top_dir + " --save " + save_dir))
