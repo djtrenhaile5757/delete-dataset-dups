7cfd1e0f6414200c8cac1eb3ca2f5c0c1ed64b54
Danvers Trenhaile <45981790+djtrenhaile5757@users.noreply.github.com>
2018-12-26 12:35:15 -0600

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import shlex as sh
 
 
 path = #path_to_directories
-top_dir = path + "test" 
-save_dir = path + "save"
+top_dir = path + "" #path_to_dataset 
+save_dir = path + "" #path_to_save_directory
 
 s.call(sh.split("python main.py --directory " + top_dir + " --save " + save_dir))
